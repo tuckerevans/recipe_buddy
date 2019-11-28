4a7b855e85eb9e1dd04c7d9b0ede1f387bee31bc
Tucker Evans <tuckerevans24@gmail.com>
2019-11-28 07:55:00 -0500

    Fix error handling on database connect
    Now panics when can't connect to database and splits handling of errors
    from Open() and Ping() as separate issues.

--- backend/main.go
+++ backend/main.go
@@ -317,8 +317,13 @@ func main() {
 	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
 		DB_USER, DB_PASSWORD, DB_NAME)
 	db, err = sql.Open("postgres", dbinfo)
-	if err != nil || db.Ping() != nil {
-		fmt.Println("Error connecting to database")
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
 	}
 
 	http.HandleFunc("/recipes", RecipeList)
