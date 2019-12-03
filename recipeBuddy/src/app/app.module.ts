33ff048f5efb30e767780f24424588af562ea400
Jacob <gatelyjm@clarkson.edu>
2019-12-03 08:58:29 -0500

    Added new recipe card and main page components. Added new files in each of these components for html, css, ts, and spec.ts. HTML and CSS files were updated with code from stackblitz. Apparently this thing also thinks I changed the app.module.ts file, but I do not think I did.

--- recipeBuddy/src/app/app.module.ts
+++ recipeBuddy/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { RecipeCardComponent } from './recipe-card/recipe-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    RecipeCardComponent
   ],
   imports: [
     BrowserModule
