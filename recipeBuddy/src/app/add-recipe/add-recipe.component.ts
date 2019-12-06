994f8ee685d9db04d575ceabee869b9e948684c2
Tucker Evans <tuckerevans24@gmail.com>
2019-12-06 15:17:32 -0500

    Add convert form data to Recipe Object

--- recipeBuddy/src/app/add-recipe/add-recipe.component.ts
+++ recipeBuddy/src/app/add-recipe/add-recipe.component.ts
@@ -5,6 +5,12 @@ import { FormControl } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
 import { FormArray } from '@angular/forms';
 
+import { Recipe } from '../DataModels/recipe';
+import { Ingredient } from '../DataModels/ingredient'
+import { Step } from '../DataModels/step';
+import { BackendService } from '../REST_service/backend.service';
+
+
 @Component({
   selector: 'app-add-recipe',
   templateUrl: './add-recipe.component.html',
@@ -34,7 +40,9 @@ export class AddRecipeComponent {
     tags: [''],
     photos: ['']
   });
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+              private restService: BackendService
+              ) { }
 
   ngOnInit() {
   }
@@ -64,5 +72,41 @@ export class AddRecipeComponent {
       timer: ['']
       })
   );
+  }
+
+  onSubmit() {
+    console.log('In Submit')
+    var formData = this.recipeForm.value;
+
+    var ingredients = []
+    var i;
+    for  (i = 0; i < formData.ingredients.length; i++) {
+      ingredients.push(new Ingredient(formData.ingredients[0].ingrName,
+                                      formData.ingredients[0].amount,
+                                      formData.ingredients[0].unit,
+                                      ""
+                       ));
+    }
+
+    var steps = []
+    for  (i = 0; i < formData.ingredients.length; i++) {
+      steps.push(new Step(formData.steps[0].instruct,
+                          formData.steps[0].timer
+                       ));
+    }
+
+    var recipe = new Recipe (0,                    //id
+                             formData.recipeName,  //name
+                             formData.desc,        //description
+                             ingredients,          //ingredients
+                             steps,                //steps
+                             formData.servingSize, //servingSize
+                             formData.cookTime,    //cookTime
+                             0,                    //timesCooked
+                             0,                    //rating
+                             formData.tags,        //tags
+                             formData.photos       //photos
+                             );
+    console.log(JSON.stringify(recipe))
   }
 }
