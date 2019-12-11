2ce49662e65fa0a3bdd2a2681d02586c30c4f431
schencej <schencej@clarkson.edu>
2019-12-10 22:23:25 -0500

    added ingredient scaling and styling to pre cook page

--- recipeBuddy/src/app/pre-cook-pop-up/pre-cook-pop-up.component.ts
+++ recipeBuddy/src/app/pre-cook-pop-up/pre-cook-pop-up.component.ts
@@ -1,24 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import { RecipePassService } from '../RecipePass/recipe-pass.service';
+import { RecipePassService } from '../recipePass/recipe-pass.service';
 import { Recipe } from '../DataModels/recipe';
+import { Ingredient } from '../DataModels/ingredient';
 import { BackendService } from '../REST_service/backend.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pre-cook-pop-up',
   templateUrl: './pre-cook-pop-up.component.html',
   styleUrls: ['./pre-cook-pop-up.component.css']
+//  providers: [RecipePassService]
 })
 export class PreCookPopUpComponent implements OnInit {
 
   cookedRecipe: Recipe;
+  newSize: number;
+  originalAmounts: number[] = [];
+  originalSize: number;
 
-  constructor(private recipePass: RecipePassService, private backend: BackendService) { }
+  constructor(private recipePass: RecipePassService, private backend: BackendService, private router: Router) { }
 
   ngOnInit() {
-	this.backend.getRecipe(1).subscribe(res =>
+	this.backend.getRecipe(15).subscribe(res =>
 	{
-		this.cookedRecipe = res
+		console.log(res);
+		this.cookedRecipe = res;
+		this.originalSize = this.cookedRecipe.servingSize;
+		for(var _i = 0; _i < this.cookedRecipe.ingredients.length; _i++) {
+			this.originalAmounts[_i] = this.cookedRecipe.ingredients[_i].amount;
+		}
 	});
-	//this.recipeToBeCooked = this.recipePass.getRecipe();
+  }
+
+  updateRecipe(event: any) {
+	if(event.target.value) {
+		this.newSize = parseInt(event.target.value);
+		for(var _i = 0; _i < this.cookedRecipe.ingredients.length; _i++) {
+			this.cookedRecipe.ingredients[_i].amount = Math.round(this.originalAmounts[_i] * (this.newSize / this.originalSize));
+		}
+
+		this.cookedRecipe.servingSize = this.newSize;
+		this.recipePass.setRecipe(this.cookedRecipe);
+	}
   }
 }
