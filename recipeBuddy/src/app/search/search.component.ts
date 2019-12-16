26d952fdf5f79f6c48aadab1f31e0bc03ac32c36
Tucker Evans <tuckerevans24@gmail.com>
2019-12-16 10:19:46 -0500

    Add initial functionality to frontend search
    Does not display recipes yet.

--- recipeBuddy/src/app/search/search.component.ts
+++ recipeBuddy/src/app/search/search.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { Recipe } from '../DataModels/recipe';
+import { BackendService } from '../REST_service/backend.service';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +11,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  searchForm = this.fb.group(
+    {searchField: ['', [Validators.required, Validators.pattern("^[A-Z0-9a-z ]*$")]]}
+  )
 
-  constructor() { }
+  constructor(private restService: BackendService,
+              private fb: FormBuilder,
+              ) {}
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    var searchData = this.searchForm.value;
+    var query = searchData.searchField;
+    query = query.replace("/ */g", "+");
+
+    console.log("QUERY: " + query);
+
+  this.restService.searchRecipes(query).subscribe(
+    res => {
+      console.log("RECIPES: " + res);
+    },
+    err => {
+    },
+    () => {
+    }
+  )
   }
 
 }
